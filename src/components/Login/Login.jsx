daea820ccfb0bf0310851928c0281d1728935c0b
muri0222 <murielacostahtml@gmail.com>
2024-01-18 16:30:02 -0300

    Projeto Salvador Tech

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import './estiloLogin.css'
+
+export default function Login( ){
+    return (
+<div className='login'>
+<div>
+    <label htmlFor="">Login:<input type='email'></input></label>
+
+</div>
+<div>
+<label htmlFor="">Senha:<input type='password'></input></label>
+</div>
+
+</div>
+
+    )
+}
